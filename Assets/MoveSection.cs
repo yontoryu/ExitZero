07d8b689d2291c72b8fc9ee8016bab50f4038f1f
yontoryu <157221570+yontoryu@users.noreply.github.com>
2025-04-01 20:09:15 +0200

    changed how the sections move

--- Assets/MoveSection.cs
+++ Assets/MoveSection.cs
@@ -2,6 +2,8 @@
 
 public class MoveSection : MonoBehaviour
 {
+    public float velocity = 15;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,13 +13,17 @@ public class MoveSection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(15, 0, 0) * Time.deltaTime;
+        transform.position += new Vector3(velocity, 0, 0) * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Destroy")) {
+        if (other.gameObject.CompareTag("Destroy")) {
             Destroy(gameObject);
+        }
+        if (other.gameObject.CompareTag("Player")) {
+            Instantiate(gameObject, new Vector3(-28.45037f, 0, 0), Quaternion.identity);
+            Debug.Log("test");
         }
     }
 }
